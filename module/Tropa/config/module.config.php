b21daac0dc73b4b21b14feca34cff1106ce64a25
Augusto Zeponi <augusto.zeponi@benner.com.br>
2018-06-12 15:29:52 -0300

    #223 - Criação das rotas

---
+++ module/Tropa/config/module.config.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Tropa;
+
+use Zend\Router\Http\Literal;
+use Zend\Router\Http\Segment;
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'router' => [
+        'routes' => [
+            'tropa' => [
+                'type' => Segment::class,
+                'options' => [
+                    'route'    => '/tropa[/:controller[/:action]]',
+                    'defaults' => [
+                        'controller' => Controller\IndexController::class,
+                        'action'     => 'index',
+                    ],
+                    'constraints' => [
+                        'action' => '[a-zA-Z0-9_-]+',
+                        'id' => '[0-9]+'
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'controllers' => [
+        'aliases' => [
+            'setor' => Controller\SetorController::class
+        ],
+        'factories' => [
+            Controller\IndexController::class => InvokableFactory::class,
+            Controller\SetorController::class => InvokableFactory::class
+        ],
+    ],
+    'view_manager' => [
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => [
+            'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
+            'tropa/index/index' => __DIR__ . '/../view/tropa/index/index.phtml',
+            'error/404'               => __DIR__ . '/../view/error/404.phtml',
+            'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ],
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+];
